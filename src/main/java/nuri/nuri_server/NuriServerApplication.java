e9b71651d1a07d52396e85155d465c93c52a4a02
lian2945 <lian080408@gmail.com>
2025-07-09 08:25:46 +0900

    Initial Commit

---
+++ src/main/java/nuri/nuri_server/NuriServerApplication.java
@@ -0,0 +1,13 @@
+package nuri.nuri_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuriServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nuri/nuri_server/NuriServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nuri.nuri_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
