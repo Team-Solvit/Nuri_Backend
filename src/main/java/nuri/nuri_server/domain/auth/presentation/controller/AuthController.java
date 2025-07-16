fc3f5383cd7ea400b045dacd5b6b3803634038fe
huhonmcmc <huhonmcmc@gmail.com>
2025-07-16 10:09:54 +0900

    SOLVIT25-43 :: global 설정 완료후 커밋

---
+++ src/main/java/nuri/nuri_server/domain/auth/presentation/controller/AuthController.java
@@ -0,0 +1,55 @@
+package nuri.nuri_server.domain.auth.presentation.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import nuri.nuri_server.domain.auth.application.service.AuthService;
+import nuri.nuri_server.domain.auth.presentation.dto.req.LoginRequest;
+import nuri.nuri_server.domain.auth.presentation.dto.req.SignupRequest;
+import nuri.nuri_server.domain.auth.presentation.dto.res.TokenResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest signupRequest) {
+        authService.signup(signupRequest);
+        return ResponseEntity.ok("회원가입에 성공하였습니다.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
+        TokenResponse tokenResponse = authService.login(loginRequest);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenResponse.accessToken())
+                .header(HttpHeaders.SET_COOKIE, tokenResponse.refreshTokenCookie())
+                .body("로그인에 성공하였습니다.");
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<String> reissue(HttpServletRequest request) {
+        TokenResponse tokenResponse = authService.reissue(request);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenResponse.accessToken())
+                .header(HttpHeaders.SET_COOKIE, tokenResponse.refreshTokenCookie())
+                .body("토큰 재발급에 성공하였습니다.");
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        String refreshTokenCookie = authService.logout(request);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer ")
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie)
+                .body("로그아웃에 성공하였습니다.");
+    }
+}
