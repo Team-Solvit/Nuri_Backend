91bff19a12e9100cd75de1c7c17da9350f445fcd
lian08 <lian080408@gmail.com>
2025-07-19 09:33:57 +0900

    SOLVIT25-43 :: 코드래빗 피드백 반영

--- src/main/java/nuri/nuri_server/domain/auth/presentation/dto/req/SignupRequest.java
+++ src/main/java/nuri/nuri_server/domain/auth/presentation/dto/req/SignupRequest.java
@@ -28,29 +28,29 @@ public record SignupRequest(
 
     @AssertTrue(message = "서비스 이용약관에 동의해야 합니다.")
     @JsonProperty("agreed_terms_of_service")
-    Boolean agreedTermsOfService,
+    boolean agreedTermsOfService,
 
     @AssertTrue(message = "개인정보 수집 이용 동의에 동의해야 합니다.")
     @JsonProperty("agreed_privacy_collection")
-    Boolean agreedPrivacyCollection,
+    boolean agreedPrivacyCollection,
 
     @AssertTrue(message = "개인정보 제 3자 제공 동의에 동의해야 합니다.")
     @JsonProperty("agreed_privacy_third_party")
-    Boolean agreedPrivacyThirdParty,
+    boolean agreedPrivacyThirdParty,
 
     @AssertTrue(message = "본인 확인 서비스 이용약관에 동의해야 합니다.")
     @JsonProperty("agreed_identity_agency_terms")
-    Boolean agreedIdentityAgencyTerms,
+    boolean agreedIdentityAgencyTerms,
 
     @AssertTrue(message = "개인정보 수집 이용 및 위탁 동의에 동의해야 합니다.")
     @JsonProperty("agreed_identity_privacy_delegate")
-    Boolean agreedIdentityPrivacyDelegate,
+    boolean agreedIdentityPrivacyDelegate,
 
     @AssertTrue(message = "고유식별정보 처리 동의 사항에 동의해야 합니다.")
     @JsonProperty("agreed_identity_unique_info")
-    Boolean agreedIdentityUniqueInfo,
+    boolean agreedIdentityUniqueInfo,
 
     @AssertTrue(message = "본인확인 서비스 이용약관에 동의해야 합니다.")
     @JsonProperty("agreed_identity_provider_terms")
-    Boolean agreedIdentityProviderTerms
+    boolean agreedIdentityProviderTerms
 ) {}
--- src/main/java/nuri/nuri_server/domain/user/domain/repository/LanguageRepository.java
+++ src/main/java/nuri/nuri_server/domain/user/domain/repository/LanguageRepository.java
@@ -2,12 +2,18 @@ package nuri.nuri_server.domain.user.domain.repository;
 
 import nuri.nuri_server.domain.user.domain.entity.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, UUID> {
     Optional<Language> findByName(String name);
+    @Query("select l from Language l where l.name in :languages")
+    @Modifying
+    List<Language> findByNameIn(List<String> languages);
 }
