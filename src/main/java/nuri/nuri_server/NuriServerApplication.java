1915a52383cda96b0bc4a92f44173d8a765e7ff3
lian08 <lian080408@gmail.com>
2025-08-14 03:14:03 +0900

    MAIN :: Test

--- src/main/java/nuri/nuri_server/NuriServerApplication.java
+++ src/main/java/nuri/nuri_server/NuriServerApplication.java
@@ -1,12 +1,20 @@
 package nuri.nuri_server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class NuriServerApplication {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(NuriServerApplication.class, args);
+        Environment env = ctx.getEnvironment();
+        String url = env.getProperty("spring.datasource.url");
+        log.info(url);
         SpringApplication.run(NuriServerApplication.class, args);
     }
 
