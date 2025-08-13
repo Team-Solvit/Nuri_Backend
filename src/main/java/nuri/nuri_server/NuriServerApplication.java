cfedc0c4b757f5b82c3cbef21815ec550b24fae0
lian08 <lian080408@gmail.com>
2025-08-14 03:02:50 +0900

    MAIN :: Test

--- src/main/java/nuri/nuri_server/NuriServerApplication.java
+++ src/main/java/nuri/nuri_server/NuriServerApplication.java
@@ -1,12 +1,23 @@
 package nuri.nuri_server;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class NuriServerApplication {
 
+    @Value("${spring.datasource.url}")
+    private static String url;
+
+    @Value("${spring.datasource.password}")
+    private static String password;
+
     public static void main(String[] args) {
+        log.info(url);
+        log.info(password);
         SpringApplication.run(NuriServerApplication.class, args);
     }
 
