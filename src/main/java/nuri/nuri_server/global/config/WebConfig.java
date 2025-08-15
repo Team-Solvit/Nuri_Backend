69e889de72d9fedbd65ebaa56ad59833cc4ad2d0
lian08 <lian080408@gmail.com>
2025-08-16 00:26:39 +0900

    MAIN :: CorsPolicy Temporary Change

--- src/main/java/nuri/nuri_server/global/config/WebConfig.java
+++ src/main/java/nuri/nuri_server/global/config/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
