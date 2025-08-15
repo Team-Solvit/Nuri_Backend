ef4e527faf340686cd3bc1435f4987f6bc8f0739
huhonmcmc <huhonmcmc@gmail.com>
2025-08-16 01:10:48 +0900

    Feat(SOLVIT25-95) :: 제3자 하숙관리 업무 기록 업로드 api 개발 완료!!!(warning 제거)

--- src/main/java/nuri/nuri_server/domain/boarding_manage/domain/entity/BoardingManageWorkEntity.java
+++ src/main/java/nuri/nuri_server/domain/boarding_manage/domain/entity/BoardingManageWorkEntity.java
@@ -1,7 +1,6 @@
 package nuri.nuri_server.domain.boarding_manage.domain.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
