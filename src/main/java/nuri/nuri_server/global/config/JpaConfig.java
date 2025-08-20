7a46762429521c218a65af547e64d74ba31f019b
Lian08 <lian080408@gmail.com>
2025-08-21 00:46:09 +0900

    DEV :: TransactionManager Bean Name Conflict Solved

--- src/main/java/nuri/nuri_server/global/config/JpaConfig.java
+++ src/main/java/nuri/nuri_server/global/config/JpaConfig.java
@@ -1,10 +1,25 @@
 package nuri.nuri_server.global.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class JpaConfig {
+    private final EntityManagerFactory entityManagerFactory;
 
+    @Primary
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
 }
