c8d57c58f0ee8b017fd38643946ac539f894759c
Lian08 <lian080408@gmail.com>
2025-08-21 01:16:08 +0900

    DEV :: OffsetDateTime으로 반환되게 변경

--- src/main/java/nuri/nuri_server/global/config/JpaConfig.java
+++ src/main/java/nuri/nuri_server/global/config/JpaConfig.java
@@ -5,9 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
@@ -21,5 +25,10 @@ public class JpaConfig {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
+    }
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
     }
 }
