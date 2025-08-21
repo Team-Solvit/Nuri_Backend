c72b6960aa08781bdfb49e0ce3f157d264f40e47
Lian08 <lian080408@gmail.com>
2025-08-21 23:55:14 +0900

    DEV :: Controller AuthenticationPrincipal Annotation Updated

--- src/main/java/nuri/nuri_server/domain/chat/presentation/controller/ChatController.java
+++ src/main/java/nuri/nuri_server/domain/chat/presentation/controller/ChatController.java
@@ -15,9 +15,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -33,53 +33,46 @@ public class ChatController {
     }
 
     @MutationMapping
-    public RoomCreateResponseDto createRoom(Principal principal, @Argument RoomCreateRequestDto roomCreateRequestDto) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public RoomCreateResponseDto createRoom(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument RoomCreateRequestDto roomCreateRequestDto) {
         log.info("createRoom 인자값 : {}, {}", nuriUserDetails.getUsername(), roomCreateRequestDto.toString());
         return chatService.createRoom(nuriUserDetails, roomCreateRequestDto);
     }
 
     @QueryMapping
-    public Page<RoomReadResponseDto> getRooms(Principal principal, @Argument @NotNull(message = "페이지 값은 존재해야 합니다.") Integer page, @Argument @NotNull(message = "사이즈 값은 존재해야 합니다.") Integer size) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public Page<RoomReadResponseDto> getRooms(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "페이지 값은 존재해야 합니다.") Integer page, @Argument @NotNull(message = "사이즈 값은 존재해야 합니다.") Integer size) {
         log.info("getRooms 인자값 : {}, {}, {}", nuriUserDetails.getUsername(), page, size);
         return chatService.getRooms(nuriUserDetails, PageRequest.of(page, size));
     }
 
     @QueryMapping
-    public List<String> getRoomsGroupChat(Principal principal) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public List<String> getRoomsGroupChat(@AuthenticationPrincipal NuriUserDetails nuriUserDetails) {
         log.info("getRoomsGroupChat 인자값 : {}", nuriUserDetails.getUsername());
         return chatService.getRoomsGroupChat(nuriUserDetails);
     }
 
     @MutationMapping
-    public Boolean invite(Principal principal, @Argument RoomInviteRequestDto roomInviteRequestDto) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public Boolean invite(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument RoomInviteRequestDto roomInviteRequestDto) {
         log.info("invite 인자값 : {}, {}", nuriUserDetails.getUsername(), roomInviteRequestDto.toString());
         chatService.invite(nuriUserDetails, roomInviteRequestDto);
         return true;
     }
 
     @MutationMapping
-    public Boolean exit(Principal principal, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public Boolean exit(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
         log.info("exit 인자값 : {}, {}", nuriUserDetails.getUsername(), roomId);
         chatService.exit(nuriUserDetails, roomId);
         return true;
     }
 
     @MutationMapping
-    public Boolean kick(Principal principal, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId, @Argument @NotNull(message = "추방시킬 유저가 존재해야 합니다.") String userId) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public Boolean kick(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId, @Argument @NotNull(message = "추방시킬 유저가 존재해야 합니다.") String userId) {
         log.info("kick 인자값 : {}, {}, {}", nuriUserDetails.getUsername(), roomId, userId);
         chatService.kick(nuriUserDetails, roomId, userId);
         return true;
     }
 
     @MutationMapping
-    public Boolean exitRoom(Principal principal, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
-        NuriUserDetails nuriUserDetails = (NuriUserDetails) principal;
+    public Boolean exitRoom(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
         log.info("exitRoom 인자값 : {}, {}", nuriUserDetails.getUsername(), roomId);
         chatService.exitRoom(nuriUserDetails, roomId);
         return true;
