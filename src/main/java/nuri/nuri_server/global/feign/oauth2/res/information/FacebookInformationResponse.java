e7048ddaed70c88eee45de3565b87cb8a509bd85
Lian08 <lian080408@gmail.com>
2025-08-22 00:35:00 +0900

    DEV :: ChatGraphQLs Updated and FaceBookInformation Update

--- src/main/java/nuri/nuri_server/global/feign/oauth2/res/information/FacebookInformationResponse.java
+++ src/main/java/nuri/nuri_server/global/feign/oauth2/res/information/FacebookInformationResponse.java
@@ -1,23 +1,18 @@
 package nuri.nuri_server.global.feign.oauth2.res.information;
 
-import lombok.Getter;
-
-@Getter
-public class FacebookInformationResponse {
-    private String id;
-    private String name;
-    private Picture picture;
-
-    @Getter
-    public static class Picture {
-        private PictureData data;
-
-        @Getter
-        public static class PictureData {
-            private int height;
-            private boolean is_silhouette;
-            private String url;
-            private int width;
-        }
+public record FacebookInformationResponse(
+        String id,
+        String name,
+        Picture picture
+) {
+    public record Picture(
+            PictureData data
+    ) {
+        public record PictureData(
+                int height,
+                boolean is_silhouette,
+                String url,
+                int width
+        ) { }
     }
 }
