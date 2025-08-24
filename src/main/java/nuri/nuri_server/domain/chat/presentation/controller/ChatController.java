b21688e225ab340bfe07118da7e6fd258b6cd977
Lian08 <lian080408@gmail.com>
2025-08-25 07:54:27 +0900

    DEV :: User Authentication Add and roomId add from getRooms

--- src/main/java/nuri/nuri_server/domain/chat/presentation/controller/ChatController.java
+++ src/main/java/nuri/nuri_server/domain/chat/presentation/controller/ChatController.java
@@ -10,6 +10,7 @@ import nuri.nuri_server.domain.chat.presentation.dto.req.RoomInviteRequestDto;
 import nuri.nuri_server.domain.chat.presentation.dto.res.ChatRecordResponseDto;
 import nuri.nuri_server.domain.chat.presentation.dto.res.RoomCreateResponseDto;
 import nuri.nuri_server.domain.chat.presentation.dto.res.RoomReadResponseDto;
+import nuri.nuri_server.global.security.annotation.User;
 import nuri.nuri_server.global.security.user.NuriUserDetails;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -26,30 +27,35 @@ import java.util.List;
 public class   ChatController {
     private final ChatService chatService;
 
+    @User
     @QueryMapping
     public List<ChatRecordResponseDto> readMessages(@Argument @NotNull(message = "방 아이디 값은 존재해야 합니다.") String roomId) {
         log.info("readMessages 인자값 : {}", roomId);
         return chatService.readMessages(roomId);
     }
 
+    @User
     @MutationMapping
     public RoomCreateResponseDto createRoom(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @Valid RoomCreateRequestDto roomCreateRequestDto) {
         log.info("createRoom 인자값 : {}, {}", nuriUserDetails.getUsername(), roomCreateRequestDto.toString());
         return chatService.createRoom(nuriUserDetails, roomCreateRequestDto);
     }
 
+    @User
     @QueryMapping
     public List<RoomReadResponseDto> getRooms(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "페이지 값은 존재해야 합니다.") Integer page, @Argument @NotNull(message = "사이즈 값은 존재해야 합니다.") Integer size) {
         log.info("getRooms 인자값 : {}, {}, {}", nuriUserDetails.getUsername(), page, size);
         return chatService.getRooms(nuriUserDetails, PageRequest.of(page, size));
     }
 
+    @User
     @QueryMapping
     public List<String> getRoomsGroupChat(@AuthenticationPrincipal NuriUserDetails nuriUserDetails) {
         log.info("getRoomsGroupChat 인자값 : {}", nuriUserDetails.getUsername());
         return chatService.getRoomsGroupChat(nuriUserDetails);
     }
 
+    @User
     @MutationMapping
     public Boolean invite(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @Valid RoomInviteRequestDto roomInviteRequestDto) {
         log.info("invite 인자값 : {}, {}", nuriUserDetails.getUsername(), roomInviteRequestDto.toString());
@@ -57,6 +63,7 @@ public class   ChatController {
         return true;
     }
 
+    @User
     @MutationMapping
     public Boolean exit(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
         log.info("exit 인자값 : {}, {}", nuriUserDetails.getUsername(), roomId);
@@ -64,6 +71,7 @@ public class   ChatController {
         return true;
     }
 
+    @User
     @MutationMapping
     public Boolean kick(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId, @Argument @NotNull(message = "추방시킬 유저가 존재해야 합니다.") String userId) {
         log.info("kick 인자값 : {}, {}, {}", nuriUserDetails.getUsername(), roomId, userId);
@@ -71,6 +79,7 @@ public class   ChatController {
         return true;
     }
 
+    @User
     @MutationMapping
     public Boolean exitRoom(@AuthenticationPrincipal NuriUserDetails nuriUserDetails, @Argument @NotNull(message = "방 아이디는 존재해야 합니다.") String roomId) {
         log.info("exitRoom 인자값 : {}, {}", nuriUserDetails.getUsername(), roomId);
--- src/main/java/nuri/nuri_server/domain/chat/presentation/dto/common/RoomDto.java
+++ src/main/java/nuri/nuri_server/domain/chat/presentation/dto/common/RoomDto.java
@@ -4,14 +4,19 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import nuri.nuri_server.domain.chat.domain.entity.RoomEntity;
 
+import java.util.UUID;
+
 @Builder
 public record RoomDto(
+        @NotBlank(message = "아이디는 존재해야 합니다.")
+        UUID id,
         @NotBlank(message = "이름이 존재해야 합니다.")
         String name,
         String profile
 ) {
     public static RoomDto from(RoomEntity roomEntity) {
         return RoomDto.builder()
+                .id(roomEntity.getId())
                 .name(roomEntity.getName())
                 .profile(roomEntity.getProfile())
                 .build();
