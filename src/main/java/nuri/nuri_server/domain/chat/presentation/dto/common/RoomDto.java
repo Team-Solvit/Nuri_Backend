b71cd4a23736f5762b9894b3306ddfbff46db7af
Lian08 <lian080408@gmail.com>
2025-08-25 07:59:24 +0900

    DEV :: ChatGraphQLS Updated

--- src/main/java/nuri/nuri_server/domain/chat/presentation/dto/common/RoomDto.java
+++ src/main/java/nuri/nuri_server/domain/chat/presentation/dto/common/RoomDto.java
@@ -4,19 +4,17 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import nuri.nuri_server.domain.chat.domain.entity.RoomEntity;
 
-import java.util.UUID;
-
 @Builder
 public record RoomDto(
         @NotBlank(message = "아이디는 존재해야 합니다.")
-        UUID id,
+        String id,
         @NotBlank(message = "이름이 존재해야 합니다.")
         String name,
         String profile
 ) {
     public static RoomDto from(RoomEntity roomEntity) {
         return RoomDto.builder()
-                .id(roomEntity.getId())
+                .id(roomEntity.getId().toString())
                 .name(roomEntity.getName())
                 .profile(roomEntity.getProfile())
                 .build();
